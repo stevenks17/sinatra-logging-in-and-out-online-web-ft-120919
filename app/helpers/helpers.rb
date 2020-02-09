bc96910f8fe3c94dc1c20f18396fea31bbb36863
Steven K <stevenks17@gmail.com>
2020-02-09 19:40:32 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+
+   def self.current_user(session_hash)
+     @user = User.find(session)hash[:user_id])
+   end
+   
+   
+   def self.is_logged_in?(session_hash)
+     !!sessopm_hash[:user_id]
+   end
+
+
+
 end
